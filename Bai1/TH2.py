b70af39b04921171c99849d61c7ba5aff6bf602c
ThanhTung <thanhtungtb.ptit@gmail.com>
2025-10-22 07:51:45 +0700

    TH trên lớp

--- Bai1/TH2.py
+++ Bai1/TH2.py
@@ -2,3 +2,7 @@ name ="Python"
 age = 30
 formatted_string = ("My name is {} ! I'm {} years old." .format(name,age))
 print(formatted_string) 
+
+name=(str,input("Nhập tên của bạn : ").split())
+age=(int,input("Nhập tuổi của bạn : ").split())
+print ("Hello" ,name.title(), "you are ",age," years old." .format(name,age))
